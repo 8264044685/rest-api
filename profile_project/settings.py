7e9b7af6d210536317acb728cad54282152ff9d0
Dabhi <parasdabhi1996@gmail.com>
2020-02-07 16:22:21 +0530

    config

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^mpkvp!o+1vyly!rofw$(*5g*qgu5&44z)$nviafi*jasnlyci'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = ['*']
 
